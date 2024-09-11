ff54942d1a255c2bdd45bd1039db7dcc7a6503ff
FSSCoding <brett@foxsoftwaresolutions.com.au>
2024-09-11 17:10:25 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 document.getElementById('changeTextButton').addEventListener('click', function() {
     
     const turnText = document.getElementById('turnText');
-    
     var changeTextButton = document.getElementById('changeTextButton');
-
+    var headingText = document.getElementById('headingText');
+    var descriptionText = document.getElementById('descriptionText');
+    
     if (turnText.innerText == "Brett's Turn!") {
         
         turnText.innerText = "Dans's Turn!";
@@ -12,8 +13,13 @@ document.getElementById('changeTextButton').addEventListener('click', function()
     else {
         
         turnText.innerText = "Brett's Turn!";
+        
+        descriptionText.style.color = "red";
+        
+        //This class adding doesn't seem to work
         turnText.classList.add('brett-text');
         changeTextButton.classList.add('brett-button');
+
     }
     
 });
