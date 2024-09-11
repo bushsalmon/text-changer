ce359023a66de19b64f91545e08e59873dd2ab73
FSSCoding <brett@foxsoftwaresolutions.com.au>
2024-09-11 16:48:39 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 document.getElementById('changeTextButton').addEventListener('click', function() {
+    
     const turnText = document.getElementById('turnText');
+    
+    var changeTextButton = document.getElementById('changeTextButton');
+
     if (turnText.innerText == "Brett's Turn!") {
+        
         turnText.innerText = "Dans's Turn!";
-    } else {
-    turnText.innerText = "Brett's Turn!";
+        
+    } 
+    else {
+        
+        turnText.innerText = "Brett's Turn!";
+        turnText.classList.add('brett-button');
+        changeTextButton.classList.add('brett-button');
     }
+    
 });
 
 
