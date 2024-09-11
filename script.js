f0bccb0589ec948d7958e08907166e57f3839f54
FSSCoding <brett@foxsoftwaresolutions.com.au>
2024-09-11 16:33:31 +1000

    Update script.js
    Added extra Dan's Turn toggle

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 document.getElementById('changeTextButton').addEventListener('click', function() {
     const turnText = document.getElementById('turnText');
+    if (turnText.innerText == "Brett's Turn!") {
+        turnText.innerText = "Dans's Turn!";
+    } else {
     turnText.innerText = "Brett's Turn!";
+    }
 });
 
 
